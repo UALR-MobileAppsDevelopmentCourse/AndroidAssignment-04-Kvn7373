76f198cabeb60cc27187a1e25d2c2690edd5c27a
KvnAguilar <kagg2234@gmail.com>
2024-03-11 23:58:26 -0500

    Almost finished with assignment 04

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -3,9 +3,12 @@ package com.ualr.layoutassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 import com.ualr.layoutassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     // TODO 02. Create a new method called "calculateTotal" for calculating the invoice's total amount of money
 
     // TODO 03. Bind the "calculateTotal" method to the button with the "CALCULATE TOTAL" label
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private ActivityMainBinding mBinding;
 
@@ -22,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mBinding.getRoot();
         setContentView(view);
+        mBinding.discountToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                if (isChecked) {
+                    //TODO 11: Get the id of the current checked button
+                    Log.d(TAG, String.format("The new checked element index is: %d", checkedId));
+                    // TODO 12: Get information about the corresponding checked button
+                    MaterialButton checkedButton = findViewById(checkedId);
+                    Log.d(TAG, String.format("The new checked element is: %s", checkedButton.getText().toString()));
+                }
+            }
+        });
     }
 }
