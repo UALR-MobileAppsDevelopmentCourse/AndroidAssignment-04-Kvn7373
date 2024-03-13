e46e0809e569b8cc396c02507ba101fe26fe8e3b
KvnAguilar <kagg2234@gmail.com>
2024-03-12 23:49:39 -0500

    Finished with assignment 04

--- app/src/main/java/com/ualr/layoutassignment/MainActivity.java
+++ app/src/main/java/com/ualr/layoutassignment/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.android.material.checkbox.MaterialCheckBox;
 import com.ualr.layoutassignment.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,17 +29,58 @@ public class MainActivity extends AppCompatActivity {
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = mBinding.getRoot();
         setContentView(view);
-        mBinding.discountToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+        MaterialButtonToggleGroup discount_btnGroup = findViewById(R.id.discountToggle_group);
+        MaterialButton nodiscount_btn = findViewById(R.id.nodiscount_btn);
+        MaterialButton discount_btn = findViewById(R.id.discount_btn);
+        MaterialButton calculate_btn= findViewById(R.id.calc_btn);
+        Integer discount_ID = discount_btn.getId();
+        MaterialCheckBox prod1_checkbox = findViewById(R.id.product1_checkbox);
+        MaterialCheckBox prod2_checkbox = findViewById(R.id.product2_checkbox);
+        MaterialCheckBox prod3_checkbox = findViewById(R.id.product3_checkbox);
+        MaterialCheckBox prod4_checkbox = findViewById(R.id.product4_checkbox);
+        EditText numfield1 = findViewById(R.id.Input1);
+        EditText numfield2 = findViewById(R.id.Input2);
+        EditText numfield3 = findViewById(R.id.Input3);
+        EditText numfield4 = findViewById(R.id.Input4);
+        TextView totalnum = findViewById(R.id.total_number);
+
+
+        calculate_btn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
-                if (isChecked) {
-                    //TODO 11: Get the id of the current checked button
-                    Log.d(TAG, String.format("The new checked element index is: %d", checkedId));
-                    // TODO 12: Get information about the corresponding checked button
-                    MaterialButton checkedButton = findViewById(checkedId);
-                    Log.d(TAG, String.format("The new checked element is: %s", checkedButton.getText().toString()));
-                }
+            public void onClick(View view) {
+            double discount;
+            double total = 0;
+            boolean prod1checked = prod1_checkbox.isChecked();
+            boolean prod2checked = prod2_checkbox.isChecked();
+            boolean prod3checked = prod3_checkbox.isChecked();
+            boolean prod4checked = prod4_checkbox.isChecked();
+
+                if(discount_btnGroup.getCheckedButtonId()== discount_ID)
+                discount = .5;
+            else discount= 1;
+            if(prod1checked)
+                total = total + getValue(numfield1);
+            if(prod2checked)
+                    total = total + getValue(numfield2);
+            if(prod3checked)
+                    total = total + getValue(numfield3);
+            if(prod4checked)
+                    total = total + getValue(numfield4);
+            total = total * discount;
+            String finalresult = Double.toString(total);
+            totalnum.setText(finalresult);
+
+
+
             }
         });
+
+}
+
+    public Double getValue(EditText edit){
+        String numtext = edit.getText().toString();
+        double num = new Double(numtext).intValue();
+        return num;
     }
 }
+
